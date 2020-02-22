492d3ca427871c0810928d6cb78ab42b77d87873
simenravnik <ravnik.simen@gmail.com>
2020-02-22 10:21:35 +0100

    Update project structure and add basic SPA navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import Button from 'antd/es/button';
 import './App.css';
+import {Route, NavLink, HashRouter} from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <Button type="primary">Button</Button>
+        <div>
+            <HashRouter>
+                <h1>Simple SPA</h1>
+                <ul className="header">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/contact" component={Contact}/>
+                </div>
+            </HashRouter>
         </div>
     );
   }
