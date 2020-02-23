8d5c977a2f93e53475eb3f258d64659eb6796540
simenravnik <ravnik.simen@gmail.com>
2020-02-23 12:13:43 +0100

    Implement data table for manipulating with primers

--- src/views/elements/Tables.js
+++ src/views/elements/Tables.js
@@ -1,112 +1,70 @@
-import React, {Component} from 'react';
-import {
-  Card,
-  CardBody,
-  Table
-} from 'reactstrap';
+import React from 'react';
+import MaterialTable from 'material-table';
 
-class TablePage extends Component {
-  render() {
+export default function MaterialTableDemo() {
+    const [state, setState] = React.useState({
+        columns: [
+            { title: 'Primer designation', field: 'name' },
+            { title: 'Sequence', field: 'sequence' },
+            { title: 'Amplicon size (bp)', field: 'size', type: 'numeric' },
+            { title: 'Annealing temperature (°C)', field: 'temperature', type: 'numeric' },
+            { title: 'Reference', field: 'reference', type: 'numeric' },
+        ],
+        data: [
+            { name: 'erm(A)-F', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'kebab', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'erm(A)-F', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'erm(A)-F', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'erm(A)-F', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'erm(A)-F', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'neki', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'erm(A)-F', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 },
+            { name: 'erm(A)-F', sequence: 'TCTAAAAAGCATGTAAAAGAAA', size: 553, temperature: 52, reference: 17 }
+        ],
+    });
+
     return (
-        <Card>
-            <CardBody>
-                Basic Table
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                Striped Table
-                <Table striped>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                Hover Table
-                <Table hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    )
-  }
+        <MaterialTable
+            title="Editable Example"
+            columns={state.columns}
+            data={state.data}
+            editable={{
+                onRowAdd: newData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            setState(prevState => {
+                                const data = [...prevState.data];
+                                data.push(newData);
+                                return { ...prevState, data };
+                            });
+                        }, 600);
+                    }),
+                onRowUpdate: (newData, oldData) =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            if (oldData) {
+                                setState(prevState => {
+                                    const data = [...prevState.data];
+                                    data[data.indexOf(oldData)] = newData;
+                                    return { ...prevState, data };
+                                });
+                            }
+                        }, 600);
+                    }),
+                onRowDelete: oldData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            setState(prevState => {
+                                const data = [...prevState.data];
+                                data.splice(data.indexOf(oldData), 1);
+                                return { ...prevState, data };
+                            });
+                        }, 600);
+                    }),
+            }}
+        />
+    );
 }
-
-export default TablePage;
