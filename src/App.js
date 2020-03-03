250eca2106d7f5403dcef8882673c2b6591d0bac
simenravnik <ravnik.simen@gmail.com>
2020-03-03 10:41:24 +0100

    Create login layout navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import DashboardLayout from './layouts/DashboardLayout';
 import './elements/scss/styles.scss';
+import LoginLayout from "./layouts/LoginLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route component={DashboardLayout} />
+        <Route path='/home' component={DashboardLayout} />
+        <Route path='/login' component={LoginLayout} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/layouts/LoginLayout.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class LoginLayout extends Component {
+
+    render() {
+        return (
+            <div>Login layout</div>
+        );
+    }
+}
