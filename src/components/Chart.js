514d7a0022388b44ea257c4aa302a4df108d6438
simenravnik <ravnik.simen@gmail.com>
2020-03-21 11:29:17 +0100

    Add bar chart to dashboard

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import { BarChart, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Tooltip, Bar, Legend } from 'recharts';
 import Title from './Title';
-
-// Generate Sales Data
-function createData(time, amount) {
-    return { time, amount };
-}
+import Text from "recharts/lib/component/Text";
 
 const data = [
-    createData('00:00', 0),
-    createData('03:00', 300),
-    createData('06:00', 600),
-    createData('09:00', 800),
-    createData('12:00', 1500),
-    createData('15:00', 2000),
-    createData('18:00', 2400),
-    createData('21:00', 2400),
-    createData('24:00', undefined),
+    {
+        "name": "fridge1",
+        "num": 10
+    },
+    {
+        "name": "fridge2",
+        "num": 50
+    },
+    {
+        "name": "fridge3",
+        "num": 23
+    },
+    {
+        "name": "fridge4",
+        "num": 54
+    },
+    {
+        "name": "fridge5",
+        "num": 64
+    },
+    {
+        "name": "fridge6",
+        "num": 43
+    },
+    {
+        "name": "fridge7",
+        "num": 31
+    }
 ];
 
 export default function Chart() {
@@ -25,29 +40,15 @@ export default function Chart() {
 
     return (
         <React.Fragment>
-            <Title>Today</Title>
+            <Title><Text style={{color: theme.palette.text.primary}}>Occupancy of Refrigerators</Text></Title>
             <ResponsiveContainer>
-                <LineChart
-                    data={data}
-                    margin={{
-                        top: 16,
-                        right: 16,
-                        bottom: 0,
-                        left: 24,
-                    }}
-                >
-                    <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
-                    <YAxis stroke={theme.palette.text.secondary}>
-                        <Label
-                            angle={270}
-                            position="left"
-                            style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
-                        >
-                            Sales ($)
-                        </Label>
-                    </YAxis>
-                    <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
-                </LineChart>
+                <BarChart width={730} height={250} data={data}>
+                    <XAxis dataKey="name"  stroke={theme.palette.text.secondary}/>
+                    <YAxis stroke={theme.palette.text.secondary} />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="num" fill="#8884d8" />
+                </BarChart>
             </ResponsiveContainer>
         </React.Fragment>
     );
