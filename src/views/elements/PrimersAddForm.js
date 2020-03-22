0a32cd638178efba5ebfa6e4a24f166477ade769
simenravnik <ravnik.simen@gmail.com>
2020-03-22 16:28:56 +0100

    Implement passing data into dialog and changing on edit

--- src/views/elements/PrimersAddForm.js
+++ src/views/elements/PrimersAddForm.js
@@ -6,6 +6,19 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from "@material-ui/core/Grid";
+import MenuItem from "@material-ui/core/MenuItem";
+
+const orientations = [
+    {
+        value: "forward",
+        label: "Forward"
+    },
+    {
+        value: "reverse",
+        label: "Reverse"
+    }
+];
 
 export default function PrimersAddForm({open, setOpen}) {
 
@@ -13,27 +26,71 @@ export default function PrimersAddForm({open, setOpen}) {
         setOpen(false);
     };
 
+    const handleConfirm = () => {
+        // handle on submit
+        console.log(data);
+    };
+
+    const [data, setData] = React.useState({
+        orientation: '',
+        sequence: ''
+    });
+
+    const handleChange = event => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        });
+    };
+
     return (
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <Dialog fullWidth={true} maxWidth={"md"} open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
             <DialogTitle id="form-dialog-title">Create new Primer</DialogTitle>
             <DialogContent>
                 <DialogContentText>
                     This will be the form for adding primers into the database. It is yet to be implemented!
                 </DialogContentText>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="name"
-                    label="Email Address"
-                    type="email"
-                    fullWidth
-                />
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={9}>
+                        <TextField
+                            name="sequence"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="sequence"
+                            label="Sequence"
+                            value={data.sequence}
+                            onChange={handleChange}
+                            autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <TextField
+                            fullWidth
+                            name="orientation"
+                            id="orientation"
+                            variant="outlined"
+                            select
+                            label="Orientation"
+                            required
+                            value={data.orientation}
+                            onChange={handleChange}
+                            helperText="Enter primer's orientation"
+                        >
+                            {orientations.map(option => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </Grid>
+                </Grid>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="primary">
                     Cancel
                 </Button>
-                <Button onClick={handleClose} color="primary">
+                <Button onClick={handleConfirm} color="primary">
                     Add
                 </Button>
             </DialogActions>
