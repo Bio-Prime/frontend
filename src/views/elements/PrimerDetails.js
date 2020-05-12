24e0082d7b71cb85f468da4bac05fe36a6f01de4
simenravnik <ravnik.simen@gmail.com>
2020-05-12 13:37:16 +0200

    Update primer details page

--- src/views/elements/PrimerDetails.js
+++ src/views/elements/PrimerDetails.js
@@ -4,14 +4,16 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import {Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis,} from 'recharts';
 import Grid from "@material-ui/core/Grid";
 import DataTable from "../../components/DataTable";
 import Paper from "@material-ui/core/Paper";
 import clsx from "clsx";
-import {makeStyles, useTheme} from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import Title from "../../components/Title";
 import PrimersColumns from "./PrimersColumns";
+import TextField from "@material-ui/core/TextField/TextField";
+import Constants from "../pages/constants";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -25,6 +27,10 @@ const useStyles = makeStyles(theme => ({
     },
     fixedHeightTimesTwo: {
         height: 405,
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
     },
 }));
 
@@ -83,6 +89,19 @@ export default function PrimerDetails({open, setOpen, data, pairsData}) {
         setOpen(false);
     };
 
+    const [formData, setFormData] = React.useState({
+        amountAvailable: data.amountAvailable,
+        amountAvailablePackType: lowerCaseAllWordsExceptFirstLetters(data.amountAvailablePackType),
+        amountAvailablePacks: data.amountAvailablePacks
+    });
+
+
+    function lowerCaseAllWordsExceptFirstLetters(string) {
+        return string.replace(/\w\S*/g, function (word) {
+            return word.charAt(0) + word.slice(1).toLowerCase();
+        });
+    }
+
     let primerColumns = PrimersColumns.getPrimersColumns();
     let tableData = [];
     const formatSelectedPrimerData = () => {
@@ -110,29 +129,34 @@ export default function PrimerDetails({open, setOpen, data, pairsData}) {
         return data.sequence;
     };
 
-
-    const getAmountAvailable = () => {
-        return data.amountAvailable;
+    const handleChange = event => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
     };
 
-    const getDate = () => {
-        let date = new Date();
-        return date.getDay() + ". " + date.getMonth() + ". " + date.getFullYear();
+    const handleNumbers = (event) => {
+        if (!isNaN(event.target.value))
+            setFormData({
+                ...formData,
+                [event.target.name]: event.target.value,
+            });
     };
 
+    const submit = (e) => {
+        e.preventDefault();
+        data.amountAvailable = formData.amountAvailable;
+        data.amountAvailablePackType = formData.amountAvailablePackType;
+        data.amountAvailablePacks = formData.amountAvailablePacks;
 
-    const chartData = [
-        {
-            name: getDate(), nMol: getAmountAvailable(),
-        },
-    ];
-
+        console.log(data);
+    };
 
     const classes = useStyles();
 
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
     const fixedHeightPaperTimesTwo = clsx(classes.paper, classes.fixedHeightTimesTwo);
-    const theme = useTheme();
 
     return (
         <Dialog fullWidth={true} maxWidth={"xl"} open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
@@ -158,21 +182,79 @@ export default function PrimerDetails({open, setOpen, data, pairsData}) {
                             </Grid>
                             <Grid item xs={12} md={12} lg={12}>
                                 <Paper className={fixedHeightPaperTimesTwo}>
+                                    <div>
                                     <Title>Amount Available</Title>
-                                    <AreaChart
-                                        width={600}
-                                        height={300}
-                                        data={chartData}
-                                        margin={{
-                                            top: 20, right: 20, left: 0, bottom: 0,
-                                        }}
+                                    <br></br>
+                                    <form
+                                        name="amountForm"
+                                        className={classes.form}
+                                        noValidate
                                     >
-                                        <CartesianGrid strokeDasharray="3 3" />
-                                        <XAxis dataKey="name" stroke={theme.palette.text.secondary} />
-                                        <YAxis stroke={theme.palette.text.secondary} />
-                                        <Tooltip />
-                                        <Area type="monotone" dataKey="nMol" stroke="#8884d8" fill="#8884d8" />
-                                    </AreaChart>
+
+                                        <Grid container spacing={3}>
+                                            <Grid item xs={9} sm={9}>
+                                                <TextField
+                                                    name="amountAvailable"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    label="Amount Available"
+                                                    value={formData.amountAvailable}
+                                                    onChange={handleNumbers}
+                                                />
+                                            </Grid>
+                                            <Grid item xs={3} sm={3}>
+                                                <TextField
+                                                    disabled
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    label="Unit"
+                                                    value={
+                                                        formData.amountAvailablePackType === "Plate" ? "wells" : "µl"
+                                                    }
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12} sm={12}>
+                                                <TextField
+                                                    name="amountAvailablePackType"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    select
+                                                    label="Pack type"
+                                                    value={formData.amountAvailablePackType}
+                                                    onChange={handleChange}
+                                                >
+                                                    {Constants.amountAvailablePackType.map((value) => (
+                                                        <MenuItem key={value} value={value}>
+                                                            {value}
+                                                        </MenuItem>
+                                                    ))}
+                                                </TextField>
+                                            </Grid>
+                                            <Grid item xs={12} sm={12}>
+                                                <TextField
+                                                    name="amountAvailablePacks"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    label="Number of packs"
+                                                    value={formData.amountAvailablePacks}
+                                                    onChange={handleNumbers}
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12} sm={12}>
+                                                <Button
+                                                    type="submit"
+                                                    fullWidth
+                                                    variant="contained"
+                                                    color="primary"
+                                                    className={classes.submit}
+                                                    onClick={submit}
+                                                >
+                                                    Save
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                    </form>
+                                    </div>
                                 </Paper>
                             </Grid>
                         </Grid>
