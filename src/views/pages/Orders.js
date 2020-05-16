99348d3b90f1e2cf92ba9b96b5565d01a83fe02d
simenravnik <ravnik.simen@gmail.com>
2020-05-16 15:21:39 +0200

    Fix charts in orders

--- src/views/pages/Orders.js
+++ src/views/pages/Orders.js
@@ -3,27 +3,18 @@ import DataTable from "../../components/DataTable";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import clsx from "clsx";
-import {makeStyles} from "@material-ui/core/styles";
+import {makeStyles, useTheme} from "@material-ui/core/styles";
 import Title from "../../components/Title";
 import Typography from "@material-ui/core/Typography";
-import {
-    Radar,
-    RadarChart,
-    PolarGrid,
-    PolarAngleAxis,
-    PolarRadiusAxis,
-    LineChart,
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    Tooltip,
-    Legend, Line,
-} from 'recharts';
 import OrdersColumns from "../elements/OrdersColumns";
 import CustomToolbarOrdered from "../elements/OrderedToolbars/CustomToolbarOrdered";
 import CustomToolbarSelectOrdered from "../elements/OrderedToolbars/CustomToolbarSelectOrdered";
 import CustomToolbarWanted from "../elements/WantedToolbars/CustomToolbarWanted";
 import CustomToolbarSelectWanted from "../elements/WantedToolbars/CustomToolbarSelectWanted";
+import PieChart from "recharts/lib/chart/PieChart";
+import Pie from "recharts/lib/polar/Pie";
+import ResponsiveContainer from "recharts/lib/component/ResponsiveContainer";
+import {Bar, BarChart, Legend, Tooltip, XAxis, YAxis} from "recharts";
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -98,57 +89,51 @@ export default function Orders() {
         )
     };
 
-
-    const chartData = [
-        {
-            subject: 'Šimen Ravnik', A: 120, B: 110, fullMark: 150,
-        },
-        {
-            subject: 'Janez Novak', A: 98, B: 130, fullMark: 150,
-        },
-        {
-            subject: 'Metka Novak', A: 86, B: 130, fullMark: 150,
-        },
-        {
-            subject: 'Franci na Balanci', A: 99, B: 100, fullMark: 150,
-        },
-        {
-            subject: 'Janez Kranjski', A: 85, B: 90, fullMark: 150,
-        },
-        {
-            subject: 'Rudolf Maister', A: 65, B: 85, fullMark: 150,
-        },
-    ];
-
-    const dataChartTwo = [
-        {
-            name: '1. 1. 2020', num: 10,
-        },
-        {
-            name: '11. 2. 2020', num: 25,
-        },
-        {
-            name: '15. 2. 2020', num: 3,
-        },
-        {
-            name: '20. 3. 2020', num: 12,
-        },
-        {
-            name: '28. 3. 2020', num: 18,
-        },
-        {
-            name: '30. 3. 2020', num: 10,
-        },
-        {
-            name: '15. 4. 2020', num: 22,
-        },
-    ];
-
     const classes = useStyles();
 
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
     const fixedHeightPaperTwo = clsx(classes.paper, classes.fixedHeightTwo);
     const fixedHeightPaperThree = clsx(classes.paper, classes.fixedHeightThree);
+
+    const dataOrderedBy = [
+        {
+            "name": "person1",
+            "value": 400
+        },
+        {
+            "name": "person2",
+            "value": 300
+        },
+        {
+            "name": "person3",
+            "value": 500
+        },
+        {
+            "name": "person4",
+            "value": 150
+        },
+        {
+            "name": "person5",
+            "value": 200
+        },
+    ];
+
+    const dataSupplier = [
+        {
+            "name": "Supplier1",
+            "value": 40,
+        },
+        {
+            "name": "Supplier2",
+            "value": 20,
+        },
+        {
+            "name": "Supplier3",
+            "value": 120,
+        },
+    ];
+
+    const theme = useTheme();
 
     return (
         <Grid container spacing={3}>
@@ -156,12 +141,14 @@ export default function Orders() {
             <Grid item xs={12} md={6} lg={6}>
                 <Paper className={fixedHeightPaperTwo}>
                     <Title>Ordered by</Title>
-                    <RadarChart cx={260} cy={200} outerRadius={150} width={500} height={400} data={chartData}>
-                        <PolarGrid />
-                        <PolarAngleAxis dataKey="subject" />
-                        <PolarRadiusAxis />
-                        <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-                    </RadarChart>
+                    <ResponsiveContainer width="100%">
+                        <PieChart>
+                            <Pie data={dataOrderedBy} nameKey="name" dataKey="value"
+                                 label={({index, value}) => {return dataOrderedBy[index].name + " (" + value + ")"}}
+                                 cx="50%" cy="50%" outerRadius={'80%'} fill="#8884d8"
+                            />
+                        </PieChart>
+                    </ResponsiveContainer>
                 </Paper>
             </Grid>
             <Grid item xs={12} md={6} lg={6}>
@@ -179,22 +166,17 @@ export default function Orders() {
                     </Grid>
                     <Grid item xs={12} md={12} lg={12}>
                         <Paper className={fixedHeightPaperThree}>
-                            <Title>Orders through time</Title>
-                            <LineChart
-                                width={520}
-                                height={200}
-                                data={dataChartTwo}
-                                margin={{
-                                    top: 20, right: 30, left: 0, bottom: 0,
-                                }}
-                            >
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="name" />
-                                <YAxis />
-                                <Tooltip />
-                                <Legend />
-                                <Line type="monotone" dataKey="num" stroke="#8884d8" activeDot={{ r: 8 }} />
-                            </LineChart>
+                            <Title>Suppliers</Title>
+                            <br />
+                            <ResponsiveContainer width="90%">
+                                <BarChart data={dataSupplier}>
+                                    <XAxis dataKey="name"  stroke={theme.palette.text.secondary}/>
+                                    <YAxis stroke={theme.palette.text.secondary} />
+                                    <Tooltip />
+                                    <Legend />
+                                    <Bar dataKey="value" fill="#8884d8" />
+                                </BarChart>
+                            </ResponsiveContainer>
                         </Paper>
                     </Grid>
                 </Grid>
