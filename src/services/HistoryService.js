cc6fd0c1dc8114a4964ad4f307e38d6e36c98a7d
simenravnik <ravnik.simen@gmail.com>
2020-06-06 09:37:32 +0200

    Fix history

--- src/services/HistoryService.js
+++ src/services/HistoryService.js
@@ -5,7 +5,7 @@ const path = "/history";
 export default {
     async getAll() {
         try {
-            const data = await DataFetcher.get(path);
+            const data = await DataFetcher.get(path+"/all");
             return data;
         }
         catch (error) {
