12b4ba43be67226c834c6145cacd7fc68c74a1ce
UrbanKocmut <u.kocmut@gmail.com>
2020-06-15 00:38:06 +0200

    dinamicly generate address

--- src/services/Address.js
+++ src/services/Address.js
@@ -1,6 +1,9 @@
 // export default "http://83.212.82.248:8080";
 // export default "http://LOCALHOST:8080";
-export default "http://backend:8080";
+
+const ADDRESS = new URL(window.location).hostname;
+
+export default "http://"+ADDRESS+":8080";
 
 
 
