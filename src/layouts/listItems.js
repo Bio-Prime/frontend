8e073004278e54ad804d00b98705c36c772f5725
Urban Kocmut <urban@kocmut.xyz>
2020-07-30 20:18:51 +0200

    change overview icon to tabel icon

--- src/layouts/listItems.js
+++ src/layouts/listItems.js
@@ -2,38 +2,38 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import {Link} from "react-router-dom";
 import {History, SupervisorAccount} from "@material-ui/icons";
 import AddIcon from '@material-ui/icons/Add';
+import TableChartIcon from '@material-ui/icons/TableChart';
 
 export const mainListItems = (
     <div>
         <ListItem button component={Link} to="/overview">
             <ListItemIcon>
-                <DashboardIcon />
+                <TableChartIcon/>
             </ListItemIcon>
-            <ListItemText primary="Overview" />
+            <ListItemText primary="Overview"/>
         </ListItem>
         <ListItem button component={Link} to="/orders">
             <ListItemIcon>
-                <ShoppingCartIcon />
+                <ShoppingCartIcon/>
             </ListItemIcon>
-            <ListItemText primary="Orders" />
+            <ListItemText primary="Orders"/>
         </ListItem>
         <ListItem button component={Link} to="/history">
             <ListItemIcon>
-                <History />
+                <History/>
             </ListItemIcon>
-            <ListItemText primary="History" />
+            <ListItemText primary="History"/>
         </ListItem>
 
         <ListItem button component={Link} to="/add-one-general">
             <ListItemIcon>
-                <AddIcon />
+                <AddIcon/>
             </ListItemIcon>
-            <ListItemText primary="Add primer/probe" />
+            <ListItemText primary="Add primer/probe"/>
         </ListItem>
     </div>
 );
@@ -42,15 +42,15 @@ export const mainListItemsGUEST = (
     <div>
         <ListItem button component={Link} to="/overview">
             <ListItemIcon>
-                <DashboardIcon />
+                <TableChartIcon/>
             </ListItemIcon>
-            <ListItemText primary="Dashboard" />
+            <ListItemText primary="Dashboard"/>
         </ListItem>
         <ListItem button component={Link} to="/orders">
             <ListItemIcon>
-                <ShoppingCartIcon />
+                <ShoppingCartIcon/>
             </ListItemIcon>
-            <ListItemText primary="Orders" />
+            <ListItemText primary="Orders"/>
         </ListItem>
     </div>
 );
@@ -59,9 +59,9 @@ export const secondaryListItems = (
     <div>
         <ListItem button component={Link} to="/admin">
             <ListItemIcon>
-                <SupervisorAccount />
+                <SupervisorAccount/>
             </ListItemIcon>
-            <ListItemText primary="Manage users" />
+            <ListItemText primary="Manage users"/>
         </ListItem>
     </div>
 );
